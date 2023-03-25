83155ad5f89377ac64b9799c7533162419fdc011
UnaiRazkin <124682636+UnaiRazkin@users.noreply.github.com>
2023-03-25 12:41:42 +0100

    Update sarrera.f90

--- sarrera.f90
+++ sarrera.f90
@@ -4,13 +4,25 @@ use tipoak
 real(kind=dp), dimension(100):: x, v !100 partikula izango direlako 
 integer:: i, j 
 Real(kind=dp):: V0
-call random_number(x)
-call random_number(v)
 
+call random_number(x) !100 posizio aleaorio
+
+
+!Potentzialaren kalkuloa:
+
+V0=0.0_dp
 do i=1,100
 do j=1,100
-    V0=V0+((50.1-i)*(50.1-j))/abs((50.1-i)*(50.1-j))/abs(x(i)-x(j))
+    V0=V0+sinu(i,j)/abs(x(i)-x(j))
 enddo
 enddo
 V0=V0/2
+
+!__________________
+contains
+    function sinu(i,j) !potentziala + edo - izango den
+    integer, intent(in)::i,j
+    integer:: sinu
+    sinu=(-1)**(floor((i-1)/50)*floor((j-1)/50))
+    end function sinu
 end program plasma
