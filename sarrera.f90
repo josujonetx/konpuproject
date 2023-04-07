71e8deac24ae73d8c12aefb778b7a0b54f3bd010
UnaiRazkin <124682636+UnaiRazkin@users.noreply.github.com>
2023-04-07 23:30:03 +0200

    Update and rename s.f90 to sarrera.f90

--- sarrera.f90
+++ sarrera.f90
@@ -2,12 +2,20 @@ program sarrera
 
 use tipoak
 
-real(kind=dp), dimension(100):: x, r, v, a !100 partikula izango direlako 
-integer, dimension(100):: c !kargen balioak
-integer:: i, j,l,k
+real(kind=dp), dimension(:), allocatable:: x, r, v, a !100 partikula izango direlako 
+integer, dimension(:), allocatable:: c !kargen balioak
+integer:: i, j,l,k,n
 Real(kind=dp):: V0,U0,vm,w, dt, m,t
 
 open(unit=111, file="K.dat", status="replace", action="write")
+
+n=100 !pariikula kopurua
+
+allocate(x(n))
+allocate(r(n))
+allocate(v(n))
+allocate(a(n))
+allocate(c(n))
 
 !sistemaren energia
 U0=10.0_dp
@@ -17,15 +25,17 @@ potentziala: do
 
 call random_number(x) !100 posizio aleaorio [0,1)
 
-do i=1,50 !kargen balioak 1/-1
+do i=1,n/2 !kargen balioak 1/-1
     c(i)=1
-    c(i+50)=-1
+    c(i+n/2)=-1
 enddo
+
+c(n)=-1 !partikula kopurua bakoitia balitz
 
 V0=0.0_dp
 
-do i=1,100
-do j=1,100
+do i=1,n
+do j=1,n
     V0=V0-c(i)*c(j)*abs(x(i)-x(j))
 enddo
 enddo
@@ -46,7 +56,7 @@ end do potentziala
 ! Partiukula ez erlatibistak (E=1/2·m·v^2) eta eta denek m=1 onartuko dugu. 
 ! Hortaz abiaduraren bataz besteko kuadratikoa-ren erroa:
 
-vm=sqrt((U0-V0)/50)
+vm=sqrt((U0-V0)/n*2)
 
 call random_number(r) !abiaduren aleatoriotasuna bermatzeko r-[0,1)
 
@@ -54,11 +64,11 @@ r=2*r-1 ! r->(-1,1)
 
 w=sqrt(sum(r*r))
 
-v=r*vm/w*10 !100 elementuko abiaduren lista bat
+v=r*vm/w !100 elementuko abiaduren lista bat
 
 !x lista ordenatuko dugu errazagoa izan dadin ondorengo kalkuloa
-do i=1,99
-    do j=i+1,100
+do i=1,n-1
+    do j=i+1,n
         if (x(i)>x(j)) then
             m=x(i)
             x(i)=x(j)
@@ -71,12 +81,12 @@ do i=1,99
 end do
 
 ! Azelerazioa:
-do i=1,100
+do i=1,n
     a(i)=0.0_dp
     do j=1,i-1
         a(i)=a(i)+c(i)*c(j)
     enddo
-    do j=i+1,100
+    do j=i+1,n
         a(i)=a(i)-c(i)*c(j)
     enddo
 enddo
@@ -85,14 +95,14 @@ enddo
 
 do j=1,10000
     call karak(x,v,a,dt,l) !Denbora karakteristikoa (dt) eta prozesua (l)
-    do i=1,100
-        x(i)=x(i)+v(i)*dt+a(i)/2*dt**2   ! Eboluzio denborala
+    do i=1,n
+        x(i)=x(i)+v(i)*dt+(a(i)/2)*dt**2   ! Eboluzio denborala
         v(i)=v(i)+a(i)*dt
     enddo
     if (l==0) then
         v(1)=-v(1) !ezkerrako paretaren aurkako talka
-    else if (l==100) then
-        v(100)=-v(100) !eskuineko paretaren aurkako talka
+    else if (l==n) then
+        v(n)=-v(n) !eskuineko paretaren aurkako talka
     else !gurutzatzea
         m=x(l)
         x(l)=x(l+1)
@@ -108,8 +118,16 @@ do j=1,10000
         c(l+1)=m
     end if
     t=t+dt
-    write(unit=111, fmt=*) t, sum(v*v)/2, 10.0
-    
+                                
+  V0=0.0_dp
+
+    do i=1,n
+        do k=1,n
+             V0=V0-c(i)*c(k)*abs(x(i)-x(k))
+        enddo
+    enddo
+
+    write(unit=111, fmt=*) t, sum(v*v)/2, V0/2,  sum(v*v)/2+V0/2
 enddo
 
 contains
@@ -120,13 +138,19 @@ contains
         real(kind=dp):: d,b,c,t1,t2
         real(kind=dp), intent(inout)::dt
         integer, intent(inout):: l
-        
+
         dt=100.0
-        
+
             ! 1. partikula ezker paretarekin talka 
-            
+
         t1=(-v(1)-sqrt(v(1)**2-2*a(1)*x(1)))/a(1)
         t2=(-v(1)+sqrt(v(1)**2-2*a(1)*x(1)))/a(1)
+
+        if (abs(t1)<1E-9) then  !filtro bat da, batzuetan zehaztaun faltak bednbora tarte txikiegiak ematen baititu
+                t1=100.0
+        else if (abs(t2)<1E-9) then
+                t2=100.0
+        end if
 
         if ((t1<0.0_dp) .and. (t2>0.0_dp)) then
            dt=t2
@@ -141,38 +165,51 @@ contains
            dt=t1
            l=0
         end if
-            
-             ! 100. partikula eskuin paretarekin talka 
-             
-        t1=(-v(100)-sqrt(v(100)**2-2*a(100)*(-1.0+x(100))))/a(100)
-        t2=(-v(100)+sqrt(v(100)**2-2*a(100)*(-1.0+x(100))))/a(100)
-        
+
+             ! n. partikula eskuin paretarekin talka 
+
+        t1=(-v(n)-sqrt(v(n)**2-2*a(n)*(-1.0+x(n))))/a(n)
+        t2=(-v(n)+sqrt(v(n)**2-2*a(n)*(-1.0+x(n))))/a(n)
+
+        if (abs(t1)<1E-9) then
+                t1=100.0
+        else if (abs(t2)<1E-9) then
+                t2=100.0
+        end if
+
         if ((t1<0.0_dp) .and. (t2>0.0_dp) .and. (dt>t2)) then
            dt=t2
-           l=100
+        l=n
         else if ((t1>0.0_dp) .and. (t2<0.0_dp) .and. (dt>t1) ) then
            dt=t1
-           l=100
+           l=n
         else if ((t1>0.0_dp) .and. (t2>0.0_dp) .and. (t1<t2) .and. (dt>t1)) then
            dt=t1
-           l=100
+           l=n
         else if ((t1>0.0_dp) .and. (t2>0.0_dp) .and. (t1>t2) .and. (dt>t2)) then
            dt=t2
-           l=100
+           l=n
         end if
-            
+        
             !Partikulak elkar gurutzatu
-            
-        do i=1,99
+
+        do i=1,n-1
             c=x(i+1)-x(i)
             b=v(i+1)-v(i)
             d=(a(i+1)-a(i))/2
             t1=(-b-sqrt(b**2-4*d*c))/2/d
             t2=(-b+sqrt(b**2-4*d*c))/2/d
+
+            if (abs(t1)<1E-9) then
+                t1=100.0
+            else if (abs(t2)<1E-9) then
+                t2=100.0
+            end if
+
             if ((t1<0.0_dp) .and. (t2>0.0_dp) .and. (dt>t2)) then
                dt=t2
                l=i
-            else if ((t1>0.0_dp) .and. (t2<0.0_dp) .and. (dt>t1)) then
+            else if ((t1>0.0_dp) .and. (t2<0.0_dp) .and. (dt>t1) ) then
                dt=t1
                l=i
             else if ((t1>0.0_dp) .and. (t2>0.0_dp) .and. (t1<t2) .and. (dt>t1)) then
