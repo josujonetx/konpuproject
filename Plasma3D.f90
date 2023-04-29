108ee58dcbd7939494298487b9983828a65485da
UnaiRazkin <124682636+UnaiRazkin@users.noreply.github.com>
2023-04-30 00:27:14 +0200

    Update and rename Plasma3D to Plasma3D.f90

--- Plasma3D.f90
+++ Plasma3D.f90
@@ -3,15 +3,26 @@ use tipoak
 use funtzioak
 use rk4
 
-integer             :: i,j
+integer             :: i,j, l,o
 real(kind=dp)       :: t, h, U0,V0,vm,w
 real(kind=dp), dimension(50)  :: x,y,z,c,r, theta, phy,vx,vy,vz
 real(kind=dp), dimension(300) :: p
+real(kind=dp), dimension(100,1000):: K
+real(kind=dp), dimension(1000):: km
 integer, parameter  :: m=1000
 real(kind=dp), parameter :: ta=0.0_dp, tb=100.0_dp
 
+open(unit=12,file="plasma3D",status="replace",action="write")
+
+do j=1,100
+    (k(j,i)=0.0_dp, i=1,1000)
+enddo
+
+
 h=(tb-ta)/m
-t    =  0.0_dp
+
+do l=1,100
+t = 0.0_dp
 
 
 !sistemaren energia
@@ -65,13 +76,11 @@ phy=phy*acos(-1.0)       ! phy-> [0,pi)
 
 w=sqrt(sum(r*r))
 
-v=r*vm/w !100 elementuko abiaduren lista bat
+v=r*vm/w !50 elementuko abiaduren lista bat
 
 vx=v*sin(phy)*cos(theta)
 vy=v*sin(phy)*sin(theta)
 vz=v*cos(phy)
-
-open(unit=12,file="plasma3D",status="replace",action="write")
 
 do i=1,50
     p(6*i-5)= x(i)
@@ -81,11 +90,25 @@ do i=1,50
     p(6*i-1)= vy(i)
     p(6*i)  = vz(i)
 enddo
+
 do i=1,m
-        write(unit=12,fmt=*) t, p
         call rk4_paso_dp(t,p, Plasma ,h)
+        V0=0.0_dp
+
+        do j=1,50
+        do o=1,50
+            if (i/=o) then
+              V0=V0-c(j)*c(o)/sqrt((x(o)-x(j))**2+(y(o)-y(j))**2+(z(o)-z(j))**2)
+            end if
+        enddo
+        enddo
+        V0=V0/2
+        k(l,i)=sum( p(6*i-2)**2+ p(6*i-1)**2+ p(6*i)**2)/2
 end do
 
+
+
+enddo
 close(unit=12)
 
 
