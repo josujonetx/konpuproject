db1ed134f5b95126f4d045bc2ea8632e4d9c8dab
UnaiRazkin <124682636+UnaiRazkin@users.noreply.github.com>
2023-05-01 16:50:56 +0200

    Update funtzioak.f90

--- funtzioak.f90
+++ funtzioak.f90
@@ -1,7 +1,6 @@
 module funtzioak
 use tipoak
-      public:: Plasma
-      integer, public:: n    
+      public:: Plasma, Potentziala, Deribatux, Deribatuy, Deribatuz 
         
             contains
             
@@ -44,9 +43,64 @@ use tipoak
                 yprima(6*i-5)=vx(i)
                 yprima(6*i-4)=vy(i)
                 yprima(6*i-3)=vz(i)
-                yprima(6*i-2)=ax(i)
-                yprima(6*i-1)=ay(i)
-                yprima(6*i)=az(i)
+                yprima(6*i-2)=ax(i)-Deribatux(x(i),y(i),z(i), Potentziala)
+                yprima(6*i-1)=ay(i)-Deribatuy(x(i),y(i),z(i), Potentziala)
+                yprima(6*i)=az(i)-Deribatuz(x(i),y(i),z(i), Potentziala)
         enddo
         end function Plasma
+        
+        function Potentziala(x,y,z)
+            real(kind=dp), intent(in)::x,y,z
+            real(kind=dp):: Potentziala
+        
+            Potentziala=Sqrt[(6 - Sqrt[x^2 + y^2])^2 + z^2] !Donut bat
+        
+        end function Potentziala
+        
+        function Deribatux(x,y,z,f)
+        
+        real(kind=dp), intent(in)::x,y,z
+        real(kind=dp):: Deribatux
+        interface
+            function f(x,y,z)
+                real(kind=dp), intent(in):: x,y,z
+                real(kind=dp):: f
+            end function f
+        end interface
+        h=1.0E-7
+        Deribatux=(f(x+h,y,z)-f(x-h,y,z))/2/h
+        
+        end function Deribatux
+        
+        function Deribatuy(x,y,z,f)
+        
+        real(kind=dp), intent(in)::x,y,z
+        real(kind=dp):: Deribatuy
+        interface
+            function f(x,y,z)
+                real(kind=dp), intent(in):: x,y,z
+                real(kind=dp):: f
+            end function f
+        end interface
+        
+        h=1.0E-7
+        Deribatuy=(f(x,y+h,z)-f(x,y-h,z))/2/h
+        
+        end function Deribatuy
+        
+        function Deribatuz(x,y,z,f)
+        
+        real(kind=dp), intent(in)::x,y,z
+        real(kind=dp):: Deribatux
+        interface
+            function f(x,y,z)
+                real(kind=dp), intent(in):: x,y,z
+                real(kind=dp):: f
+            end function f
+        end interface
+        
+        h=1.0E-7
+        Deribatux=(f(x,y,z+h)-f(x,y,z+h))/2/h
+        
+        end function Deribatuz
 end module funtzioak
